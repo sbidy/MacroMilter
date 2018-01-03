a955af03fd7ebacdbbe2c28be59cd8f2b36a5850
sbidy <stetrau@aol.com>
2018-01-03 15:42:31 +0100

    Update testing version

--- macromilter/macromilter.py
+++ macromilter/macromilter.py
@@ -27,6 +27,7 @@
 ## 3.2 - 12.01.2017 sbidy - Fix for exceptions.UnboundLocalError, possible fix for #10 zip - extraction did not work properly
 ## 3.3 - 05.10.2017 sbidy - Update directory for FHS conformity see #13
 ## 3.4 - 27.10.2017 sbidy - Update and fix some bugs #19, #18 and #17 - create release 
+## 3.5.1 - 03.01.2018 sbidy - Fix for #31 #27 #29
 
 # The MIT License (MIT)
 
@@ -87,7 +88,7 @@ else:
 	from zipfile import ZipFile, is_zipfile
 
 ## Config see ./config.ini
-__version__ = '3.4'  # version
+__version__ = '3.5.1'  # version
 
 # get the config from FHS conform dir (bug #13)
 CONFIG = os.path.join(os.path.dirname("/etc/macromilter/"),"config.ini")
