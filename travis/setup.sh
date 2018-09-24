fe22f067ffc3d286de6915021acc1e408f376085
sbidy <stetrau@aol.com>
2018-09-24 13:13:12 +0200

    Update travis test

---
+++ travis/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# create files and folders
+mkdir /etc/macromilter/
+mkdir -p /var/log/macromilter/
+# only needed for a chroot env
+# mkdir /var/spool/postfix/etc/milter
+
+# copy the python script
+cp /home/sbidy/macromilter/macromilter.py /usr/bin/
+cp /home/sbidy/macromilter/config.ini /etc/macromilter/
+
+# setup upstart config
+cp /home/sbidy/macromilter/MacroMilter.conf /etc/init/
+initctl reload-configuration
+
+# set chown for postfix
+chown postfix:postfix -R /etc/macromilter
+chown postfix:postfix -R /var/log/macromilter
+chown postfix:postfix /usr/bin/macromilter.py
+
+# start and check
+service MacroMilter start
+tail /var/log/macromilter/macromilter.log
