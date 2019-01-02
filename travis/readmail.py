206807fabc689d9895b4da64523c757025365dec
Stephan Traub <stetrau@aol.com>
2019-01-02 16:12:28 +0100

    [skip travis]

--- travis/readmail.py
+++ travis/readmail.py
@@ -2,10 +2,6 @@
 import email
 import sys
 
-msg = ""
 for line in sys.stdin.read().split('\n'):
     print(line)
-    msg += line
-    
-f = open("/home/travis/build/sbidy/MacroMilter/travis/mail.txt", "w")
-f.write(msg) 
+ 
