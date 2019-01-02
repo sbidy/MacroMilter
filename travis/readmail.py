00927a269e6e25207b63980c772b0cd0f8bb2b5c
Stephan Traub <stetrau@aol.com>
2019-01-02 16:08:57 +0100

    Update readmail.py

--- travis/readmail.py
+++ travis/readmail.py
@@ -2,6 +2,10 @@
 import email
 import sys
 
-msg = email.message_from_file(sys.stdin)
+msg = ""
+for line in sys.stdin.read().split('\n'):
+    print(line)
+    msg += line
+    
 f = open("/home/travis/build/sbidy/MacroMilter/travis/mail.txt", "w")
 f.write(msg) 
