4446f0d8236a347a9818621bc8ddd200ac83573c
Stephan Traub <stetrau@aol.com>
2019-01-02 15:03:05 +0100

    Update mailalias.sh

--- travis/mailalias.sh
+++ travis/mailalias.sh
@@ -1,5 +1,5 @@
   #!/bin/bash
-  sudo cat /home/travis/build/sbidy/MacroMilter/travis/readmail.py
+  sudo chmod +x /home/travis/build/sbidy/MacroMilter/travis/readmail.py
   sudo echo "test: |/home/travis/build/sbidy/MacroMilter/travis/readmail.py" >> /etc/aliases
   sudo newaliases
   sudo cat /etc/aliases
