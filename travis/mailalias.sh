4498a736961caf0b019dd825bbfa02de5c31951c
Stephan Traub <stetrau@aol.com>
2019-01-02 15:42:41 +0100

    [skip travis]

--- travis/mailalias.sh
+++ travis/mailalias.sh
@@ -1,6 +1,6 @@
   #!/bin/bash
   sudo chmod 777 /home/travis/build/sbidy/MacroMilter/travis/readmail.py
-  sudo chown postfix:postfix /home/travis/build/sbidy/MacroMilter/travis/readmail.py
+  sudo chown travis:travis /home/travis/build/sbidy/MacroMilter/travis/readmail.py
   sudo echo "test: |/home/travis/build/sbidy/MacroMilter/travis/readmail.py" >> /etc/aliases
   sudo newaliases
   sudo cat /etc/aliases
